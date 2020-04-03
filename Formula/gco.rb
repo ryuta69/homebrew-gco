c6f51b7f26c97be5fc37cd02cbc6fdeaafd84634
ryuta69 <eyma22s.yu@gmail.com>
2020-04-03 14:09:32 +0900

    Feature(Formula): Upgrade Version

--- Formula/gco.rb
+++ Formula/gco.rb
@@ -1,9 +1,9 @@
 class Gco < Formula
   desc "Git Checkout with fuzzy finder skim."
   homepage "https://github.com/ryuta69/gco"
-  url "https://github.com/ryuta69/gco/releases/download/v1.0/gco-0.3.0-x86_64-apple-darwin.tar.gz"
-  sha256 "eb9c52c564c93cd1cf3edca7b6131c467bb4108c3996dcba82430ea36f40f70c"
-  version "0.1.0"
+  url "https://github.com/ryuta69/gco/releases/download/v2.0/gco-0.2.0-x86_64-apple-darwin.tar.gz"
+  sha256 "6f7d3fda375e18548e0a9082f2de0fdf51ea75d5a3b497885d853e938e82a5be"
+  version "0.2.0"
 
   def install
     bin.install "gco"
